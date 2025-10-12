df9f6bf2f727a406987f1ba6d084afbac40d7d83
Kinga <k.garbowska1@gmail.com>
2025-10-12 15:52:08 +0100

    bronze load procedure - added progress logging

--- scripts/bulk_insert_csv.sql
+++ scripts/bulk_insert_csv.sql
@@ -1,92 +1,98 @@
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	PRINT '----------------------------------------------------------------------------------------'
+	PRINT '----------------------------------------------------------------------------------------'
+	PRINT 'Loading Bronze Layer'
+	PRINT '----------------------------------------------------------------------------------------'
+	PRINT '----------------------------------------------------------------------------------------'
+	
+	PRINT '----------------------------------------------------------------------------------------'
+	PRINT 'Loading CRM Tables'
+	PRINT '----------------------------------------------------------------------------------------'
 
-TRUNCATE TABLE bronze.crm_cust_info;
+	PRINT '>> Truncating Table bronze.crm_cust_info'
+	TRUNCATE TABLE bronze.crm_cust_info;
+	PRINT '>> Inserting crm_cust_info.csv'
+	BULK INSERT bronze.crm_cust_info
+	FROM 'E:\CodeProjects\SQL-Data-Warehouse\SQL_Data_Warehouse\datasets\source_crm\cust_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	--SELECT * FROM bronze.crm_cust_info
+	--SELECT COUNT(*) FROM bronze.crm_cust_info
 
-BULK INSERT bronze.crm_cust_info
-FROM 'E:\CodeProjects\SQL-Data-Warehouse\SQL_Data_Warehouse\datasets\source_crm\cust_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT '>> Truncating Table bronze.crm_prd_info'
+	TRUNCATE TABLE bronze.crm_prd_info;
+	PRINT '>> Inserting crm_prd_info.csv'
+	BULK INSERT bronze.crm_prd_info
+	FROM 'E:\CodeProjects\SQL-Data-Warehouse\SQL_Data_Warehouse\datasets\source_crm\prd_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	--SELECT * FROM bronze.crm_prd_info
+	--SELECT COUNT(*) FROM bronze.crm_prd_info
 
-SELECT * FROM bronze.crm_cust_info
-SELECT COUNT(*) FROM bronze.crm_cust_info
+	PRINT '>> Truncating Table bronze.crm_sales_details'
+	TRUNCATE TABLE bronze.crm_sales_details;
+	PRINT '>> Inserting crm_sales_details.csv'
+	BULK INSERT bronze.crm_sales_details
+	FROM 'E:\CodeProjects\SQL-Data-Warehouse\SQL_Data_Warehouse\datasets\source_crm\sales_details.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	--SELECT * FROM bronze.crm_sales_details
+	--SELECT COUNT(*) FROM bronze.crm_sales_details
 
+		
+	PRINT '----------------------------------------------------------------------------------------'
+	PRINT 'Loading ERP Tables'
+	PRINT '----------------------------------------------------------------------------------------'
 
+	PRINT '>> Truncating Table bronze.erp_cust_az12'
+	TRUNCATE TABLE bronze.erp_cust_az12;
+	PRINT '>> Inserting erp_cust_az12.csv'
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'E:\CodeProjects\SQL-Data-Warehouse\SQL_Data_Warehouse\datasets\source_erp\cust_az12.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	--SELECT * FROM bronze.erp_cust_az12
+	--SELECT COUNT(*) FROM bronze.erp_cust_az12
 
-TRUNCATE TABLE bronze.crm_prd_info;
-
-BULK INSERT bronze.crm_prd_info
-FROM 'E:\CodeProjects\SQL-Data-Warehouse\SQL_Data_Warehouse\datasets\source_crm\prd_info.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-SELECT * FROM bronze.crm_prd_info
-SELECT COUNT(*) FROM bronze.crm_prd_info
-
-
-
-TRUNCATE TABLE bronze.crm_sales_details;
-
-BULK INSERT bronze.crm_sales_details
-FROM 'E:\CodeProjects\SQL-Data-Warehouse\SQL_Data_Warehouse\datasets\source_crm\sales_details.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-
-SELECT * FROM bronze.crm_sales_details
-SELECT COUNT(*) FROM bronze.crm_sales_details
-
-
-
-TRUNCATE TABLE bronze.erp_cust_az12;
-
-BULK INSERT bronze.erp_cust_az12
-FROM 'E:\CodeProjects\SQL-Data-Warehouse\SQL_Data_Warehouse\datasets\source_erp\cust_az12.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-
-SELECT * FROM bronze.erp_cust_az12
-SELECT COUNT(*) FROM bronze.erp_cust_az12
-
-
-
-TRUNCATE TABLE bronze.erp_loc_a101;
-
-BULK INSERT bronze.erp_loc_a101
-FROM 'E:\CodeProjects\SQL-Data-Warehouse\SQL_Data_Warehouse\datasets\source_erp\loc_a101.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-
-
-SELECT * FROM bronze.erp_loc_a101
-SELECT COUNT(*) FROM bronze.erp_loc_a101
-
-
-
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'E:\CodeProjects\SQL-Data-Warehouse\SQL_Data_Warehouse\datasets\source_erp\px_cat_g1v2.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
+	PRINT '>> Truncating Table bronze.erp_loc_a101'
+	TRUNCATE TABLE bronze.erp_loc_a101;
+	PRINT '>> Inserting loc_a101.csv'
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'E:\CodeProjects\SQL-Data-Warehouse\SQL_Data_Warehouse\datasets\source_erp\loc_a101.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	--SELECT * FROM bronze.erp_loc_a101
+	--SELECT COUNT(*) FROM bronze.erp_loc_a101
 
 
-SELECT * FROM bronze.erp_px_cat_g1v2
-SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2
+	PRINT '>> Truncating Table bronze.erp_px_cat_g1v2'
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2
+	PRINT '>> Inserting px_cat_g1v2.csv'
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'E:\CodeProjects\SQL-Data-Warehouse\SQL_Data_Warehouse\datasets\source_erp\px_cat_g1v2.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+	);
+	--SELECT * FROM bronze.erp_px_cat_g1v2
+	--SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2
+
+
+END
